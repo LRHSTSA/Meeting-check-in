c9fb065a6cb9f768fa205d2a0a5ccfbd93d06da9
LucasPMagno <lucaspmagno@gmail.com>
2018-09-05 13:00:37 -0400

    nope 521

--- src/routes.js
+++ src/routes.js
@@ -126,7 +126,7 @@ router.post('/', [
         fs.writeFileSync('./db/db.json', JSON.stringify(db));
     }
 
-    res.redirect('..')
+    res.redirect('/admin')
 })
 
 router.get('/admin', (req, res) => {
